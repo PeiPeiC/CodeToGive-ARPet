1b7ce73f15ef69682f84c685233664fb297908fb
Ophelia Chiu <104357328+PeiPeiC@users.noreply.github.com>
2023-10-07 13:23:50 +0000

    update UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,147 +1,146 @@
 import streamlit as st
 import base64
 
+try:
+    st.set_page_config(page_title="AR Pet Game", layout="centered", initial_sidebar_state="collapsed")
+except Exception:
+    pass
+
+
 # Define function to create a data URI for an image
 def image_to_data_uri(img_path):
     with open(img_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode('utf-8')
 
-# Set page title and layout
-st.set_page_config(page_title="AR Pet Game", layout="centered", initial_sidebar_state="collapsed")
-
-# Set background color and custom CSS styles
+# Set child-friendly styles
 st.markdown(
     """
     <style>
         body {
-            background-color: #FFDDC1;
+            background-color: #FFDAB9; /* Peach background */
+            font-family: 'Comic Sans MS', 'Chalkboard SE', 'Marker Felt'; /* Fun font */
+        }
+        h1, h2 {
+            color: #FF69B4; /* Bright pink titles */
         }
         .icon-button {
-            padding: 10px 20px;
+            padding: 15px 30px;
+            font-size: 20px;
             border: none;
-            background-color: #f5f5f5;
+            background-color: #ADFF2F; /* GreenYellow background for buttons */
             cursor: pointer;
             display: inline-flex;
             align-items: center;
-            gap: 10px;
-            border-radius: 5px;
+            gap: 15px;
+            border-radius: 15px;
             transition: background-color 0.3s;
         }
         .icon-button img {
-            width: 20px;
-            height: 20px;
+            width: 30px;
+            height: 30px;
         }
         .icon-button:hover {
-            background-color: #e0e0e0;
+            background-color: #7FFF00; /* Chartreuse color on hover */
         }
     </style>
     """,
     unsafe_allow_html=True,
 )
 
-# Check if user is logged in
-if 'logged_in' not in st.session_state:
-    st.session_state['logged_in'] = False
-
-# Check if user has chosen pet design
-if 'pet_design' not in st.session_state:
-    st.session_state['pet_design'] = None
-
-# Login/Signup Page
-if not st.session_state['logged_in']:
+def display_login_page():
     st.title("Welcome to AR Pet Game!")
     st.subheader("Please login or signup to continue.")
     
     # Using session state to detect button clicks
-    if 'google_clicked' not in st.session_state:
-        st.session_state['google_clicked'] = False
-    if 'facebook_clicked' not in st.session_state:
-        st.session_state['facebook_clicked'] = False
-    if 'signup_clicked' not in st.session_state:
-        st.session_state['signup_clicked'] = False
+    login_methods = ["Login with Google", "Login with Facebook", "Signup"]
+    for method in login_methods:
+        if st.button(method):
+            st.session_state['logged_in'] = True
 
-    # Display buttons and check for clicks
-    if st.button("Login with Google"):
-        st.session_state['google_clicked'] = True
-    if st.button("Login with Facebook"):
-        st.session_state['facebook_clicked'] = True
-    if st.button("Signup"):
-        st.session_state['signup_clicked'] = True
-
-    # Handle button clicks
-    if st.session_state['google_clicked']:
-        st.session_state['logged_in'] = True
-        st.session_state['google_clicked'] = False  # Reset the button state
-    if st.session_state['facebook_clicked']:
-        st.session_state['logged_in'] = True
-        st.session_state['facebook_clicked'] = False  # Reset the button state
-    if st.session_state['signup_clicked']:
-        st.session_state['logged_in'] = True
-        st.session_state['signup_clicked'] = False  # Reset the button stat
-# Pet Design Selection Page
-elif st.session_state['pet_design'] is None:
+def display_pet_design_page():
     st.title("Design Your Pet!")
     st.subheader("Choose how you'd like to design your pet.")
     
-    if st.button("Design My Own Pet"):
-        st.session_state['pet_design'] = 'custom'
-    if st.button("Use Pre-designed Pet"):
-        st.session_state['pet_design'] = 'premade'
+    pet_designs = ["Design My Own Pet", "Use Pre-designed Pet"]
+    for design in pet_designs:
+        if st.button(design):
+            st.session_state['pet_design'] = design
 
-# Input Selection or Pre-designed Pets Page
-else:
-    if st.session_state['pet_design'] == 'custom':
-        st.title("Choose Your Input Method")
-        
-        # Define the icons
-        mic_icon = image_to_data_uri("./icons/microphone_icon.png")
-        keyboard_icon = image_to_data_uri("./icons/keyboard_icon.png")
-        hand_icon = image_to_data_uri("./icons/hand_icon.png")
-        
-        col1, col2, col3 = st.columns(3)
-        
-        # Display microphone icon for voice input in the first column
-        with col1:
-            st.markdown(f"""
-            <button class="icon-button">
-                <img src="data:image/png;base64,{mic_icon}" alt="Microphone Icon">
-                Voice Input
-            </button>
-            """, unsafe_allow_html=True)
-            # Connect to Whisper API for speech-to-text (for this mockup, we'll use a text input)
-            st.text_input("Speak Now:")
-        
-        # Display keyboard icon for text input in the second column
-        with col2:
-            st.markdown(f"""
-            <button class="icon-button">
-                <img src="data:image/png;base64,{keyboard_icon}" alt="Keyboard Icon">
-                Text Input
-            </button>
-            """, unsafe_allow_html=True)
-            st.text_input("Type Here:")
-        
-        # Display hand icon for sign language input in the third column
-        with col3:
-            st.markdown(f"""
-            <button class="icon-button">
-                <img src="data:image/png;base64,{hand_icon}" alt="Hand Icon">
-                Sign Language Input
-            </button>
-            """, unsafe_allow_html=True)
-            # Placeholder for sign language input
-            st.write("Sign language input selected!")
+def display_input_method_page():
+    st.title("Choose Your Input Method")
     
-    elif st.session_state['pet_design'] == 'premade':
-        st.title("Choose Your Pet")
-        
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            if st.button("Pet 1"):
-                st.write("You chose Pet 1!")
-        with col2:
-            if st.button("Pet 2"):
-                st.write("You chose Pet 2!")
-        with col3:
-            if st.button("Pet 3"):
-                st.write("You chose Pet 3!")
+    icons = {
+        "Voice Input": "./icons/microphone_icon.png",
+        "Text Input": "./icons/keyboard_icon.png",
+        "Sign Language Input": "./icons/hand_icon.png"
+    }
+    
+    for method, icon_path in icons.items():
+        icon_data = image_to_data_uri(icon_path)
+        st.markdown(f"""
+        <button class="icon-button">
+            <img src="data:image/png;base64,{icon_data}" alt="{method}">
+            {method}
+        </button>
+        """, unsafe_allow_html=True)
+
+def display_pet_choice_page():
+    st.title("Choose Your Pet")
+    
+    pets = ["Pet 1", "Pet 2", "Pet 3"]
+    for pet in pets:
+        if st.button(pet):
+            st.write(f"You chose {pet}!")
+
+# Set child-friendly styles
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #FFDAB9; /* Peach background */
+            font-family: 'Comic Sans MS', 'Chalkboard SE', 'Marker Felt'; /* Fun font */
+        }
+        h1, h2 {
+            color: #E69966; /* Bright  titles */
+            text-align: center;
+        }
+        .icon-button {
+            padding: 15px 30px;
+            font-size: 20px;
+            border: none;
+            background-color: #FEC773; /* GreenYellow background for buttons */
+            cursor: pointer;
+            display: block; /* Make buttons block-level elements */
+            width: 80%; /* Set a fixed width */
+            margin: 10px auto; /* Center the buttons and add margin */
+            text-align: center;
+            border-radius: 15px;
+            transition: background-color 0.3s;
+        }
+        .icon-button img {
+            width: 30px;
+            height: 30px;
+        }
+        .icon-button:hover {
+            background-color: #E69966; /* Chartreuse color on hover */
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+
+# Main App Logic
+st.write("")  # Add an empty line for spacing
+col1, col2, col3 = st.columns([1, 6, 1])   # Create columns to center the content
+
+with col2:  # Use the center column to display the content
+    if not st.session_state.get('logged_in', False):
+        display_login_page()
+    elif not st.session_state.get('pet_design'):
+        display_pet_design_page()
+    else:
+        if st.session_state['pet_design'] == "Design My Own Pet":
+            display_input_method_page()
+        else:
+            display_pet_choice_page()
