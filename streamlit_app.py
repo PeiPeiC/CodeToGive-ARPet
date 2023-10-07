69f8466ffc7aab375617a4b852caba4c139de697
Ophelia Chiu <104357328+PeiPeiC@users.noreply.github.com>
2023-10-07 13:52:20 +0000

    update the openai api and UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import base64
-
-try:
-    st.set_page_config(page_title="AR Pet Game", layout="centered", initial_sidebar_state="collapsed")
-except Exception:
-    pass
-
+import openai
 
 # Define function to create a data URI for an image
 def image_to_data_uri(img_path):
     with open(img_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode('utf-8')
+
+# Load icons
+mic_icon = image_to_data_uri("./icons/microphone_icon.png")
+keyboard_icon = image_to_data_uri("./icons/keyboard_icon.png")
+hand_icon = image_to_data_uri("./icons/hand_icon.png")
 
 # Set child-friendly styles
 st.markdown(
@@ -141,6 +141,54 @@ with col2:  # Use the center column to display the content
         display_pet_design_page()
     else:
         if st.session_state['pet_design'] == "Design My Own Pet":
-            display_input_method_page()
+            st.title("Choose Your Input Method")
+            
+            # Display microphone icon for voice input
+            st.markdown(f"""
+            <button class="icon-button">
+                <img src="data:image/png;base64,{mic_icon}" alt="Microphone Icon">
+                Voice Input
+            </button>
+            """, unsafe_allow_html=True)
+            # Placeholder for voice input
+            st.text_input("Speak Now:")
+
+            # Display keyboard icon for text input
+            st.markdown(f"""
+            <button class="icon-button">
+                <img src="data:image/png;base64,{keyboard_icon}" alt="Keyboard Icon">
+                Text Input
+            </button>
+            """, unsafe_allow_html=True)
+            user_input = st.text_input("Type Here:")
+
+            if user_input:
+                st.write("Generating pet...")
+                try:
+                    openai.api_key = "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"  # Replace with your OpenAI API key
+
+                    res = openai.Image.create(
+                        prompt=user_input,
+                        n=1,
+                        size="1024x1024",
+                        response_format="b64_json"
+                    )
+
+                    generated_image_data = res['data'][0]['b64_json']
+                    image_bytes = base64.b64decode(generated_image_data)
+                    st.image(image_bytes, caption="Magic Image ", use_column_width=True)
+
+                except Exception as e:
+                    st.error("Oops! Something went wrong while generating the image. Please try again later. ")
+
+            # Display hand icon for sign language input
+            st.markdown(f"""
+            <button class="icon-button">
+                <img src="data:image/png;base64,{hand_icon}" alt="Hand Icon">
+                Sign Language Input
+            </button>
+            """, unsafe_allow_html=True)
+            # Placeholder for sign language input
+            st.write("Sign language input selected!")
         else:
             display_pet_choice_page()
