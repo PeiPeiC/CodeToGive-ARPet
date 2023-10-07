af86f4eb5155e798af84db12c46b2bee70a1afc4
Ophelia Chiu <104357328+PeiPeiC@users.noreply.github.com>
2023-10-07 09:42:12 +0000

    test app first page

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,34 +28,40 @@ st.title("Welcome to AR Pet Game!")
 # Display subtitle
 st.subheader("Choose your way to play:")
 
-# Display microphone icon for voice input
-mic_icon = image_to_data_uri("./icons/microphone_icon.jpg")
-if st.button("", on_click=None, help=None, disabled=False, use_container_width=True, key=None):
-    st.write("Voice input selected!")
-st.markdown(f'<img src="data:image/jpeg;base64,{mic_icon}" width="100">', unsafe_allow_html=True)
-st.write("Voice Input")
+# Create three columns for the buttons
+col1, col2, col3 = st.columns(3)
 
-# Display keyboard icon for text input
-keyboard_icon = image_to_data_uri("./icons/keyboard_icon.png")
-if st.button("", on_click=None, help=None, disabled=False, use_container_width=True, key=None):
-    st.write("Text input selected!")
-st.markdown(f'<img src="data:image/png;base64,{keyboard_icon}" width="100">', unsafe_allow_html=True)
-st.write("Text Input")
+# Display microphone icon for voice input in the first column
+with col1:
+    mic_icon = image_to_data_uri("./icons/microphone_icon.png")
+    if st.button("Voice Input", key="voice_button"):
+        st.write("Voice input selected!")
+    st.markdown(f'<img src="data:image/jpeg;base64,{mic_icon}" width="80" style="display: block; margin: 0 auto;">', unsafe_allow_html=True)
 
-# # Display hand icon for sign language input
-# hand_icon = image_to_data_uri("path_to_hand_icon.png")
-# if st.button("", on_click=None, help=None, disabled=False, use_container_width=True, key=None):
-#     st.write("Sign language input selected!")
-# st.markdown(f'<img src="data:image/png;base64,{hand_icon}" width="100">', unsafe_allow_html=True)
-# st.write("Sign Language Input")
+# Display keyboard icon for text input in the second column
+with col2:
+    keyboard_icon = image_to_data_uri("./icons/keyboard_icon.png")
+    if st.button("Text Input", key="text_button"):
+        st.write("Text input selected!")
+    st.markdown(f'<img src="data:image/png;base64,{keyboard_icon}" width="80" style="display: block; margin: 0 auto;">', unsafe_allow_html=True)
 
+# Display hand icon for sign language input in the third column (assuming you have this section in your code)
+with col3:
+    hand_icon = image_to_data_uri("./icons/hand_icon.png")
+    if st.button("Sign Language Input", key="sign_language_button"):
+        st.write("Sign language input selected!")
+    st.markdown(f'<img src="data:image/png;base64,{hand_icon}" width="80" style="display: block; margin: 0 auto;">', unsafe_allow_html=True)
+
+
+# Add some spacing before the footer
+st.write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+st.write("Code to give Team 1 ")
 # Display footer
 st.markdown(
     """
-    <footer style="position: absolute; bottom: 0; width: 100%; text-align: center; padding: 10px; background-color: #FFA07A;">
+    <footer style="padding: 10px; background-color: #FFA07A; text-align: center;">
         AR Pet Game &copy; 2023
     </footer>
     """,
     unsafe_allow_html=True,
 )
-
